ec1cea3e12e996a681681e772161eb2f4880c548
Valentin Panov <valentin.panov@me.com>
2021-03-08 14:12:14 +0300

    trash removed

--- src/js/parser 2.js
+++
@@ -1,8 +0,0 @@
-export default function json(data) {
-  return new Promise((resolve) => {
-    // эмуляция обработки ArrayBuffer
-    setTimeout(() => {
-      resolve(String.fromCharCode.apply(null, new Uint16Array(data)));
-    }, 500);
-  });
-}
